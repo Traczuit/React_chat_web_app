8f31f798ecdcd9c5e4d47b466f5781d9226ab370
Mauricio <traczm93@hotmail.com>
2022-02-04 21:23:55 -0300

    First commit

---
+++ chat/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Sidebar } from "./containers/Sidebar";
+
+function App() {
+  return (
+    <div id="container">
+      <aside id="sidebar">Users</aside>
+      <section id="main">
+        <section id="messages-list">Messages List</section>
+        <section id="new-message">New Message</section>
+      </section>
+    </div>
+  );
+}
+
+export default App;
